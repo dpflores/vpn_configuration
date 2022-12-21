8f41cfce797152969d898c74fe2d868cc86e4fab
Del Piero Flores <delpiero22.flores@gmail.com>
2022-12-21 12:45:52 -0500

    u

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ def main():
 	# Creamos el archivo que se ejecutará cada vez
 
 	os.system("cat > /etc/init.d/startup_script <<EOF\n /usr/bin/python3 /home/scripts/vpn_configuration/starter_vpn.py\nexit 0\nEOF")
-	os.system()
 
 if __name__ == '__main__':
 	main()
