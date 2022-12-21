ac96a891c40926d2875be2b705aba1b332c71566
Del Piero Flores <delpiero22.flores@gmail.com>
2022-12-21 16:59:51 -0500

    update

--- starter_vpn.py
+++ starter_vpn.py
@@ -7,34 +7,40 @@ VPN_SERVER_IP='137.184.105.94' #ACME VPN IP
 def main():
 
     # print("Connecting to chip")
-	os.system("ppp -c")
-	# print("connecting VPN")
+    os.system("ppp -c")
+    # print("connecting VPN")
 
-	# Do it every reboot (we'll put it in /etc/rc.local)
-	os.system("mkdir -p /var/run/xl2tpd")
-	os.system("touch /var/run/xl2tpd/l2tp-control")
+    # Do it every reboot (we'll put it in /etc/rc.local)
+    os.system("mkdir -p /var/run/xl2tpd")
+    os.system("touch /var/run/xl2tpd/l2tp-control")
 
-	os.system("service strongswan restart")
-	os.system("ipsec restart")
-	os.system("service xl2tpd restart")
+    os.system("service strongswan restart")
+    os.system("ipsec restart")
+    os.system("service xl2tpd restart")
 
-	os.system("ipsec up myvpn")
-	os.system("echo \"c myvpn\" > /var/run/xl2tpd/l2tp-control")
+    os.system("ipsec up myvpn")
+    os.system("echo \"c myvpn\" > /var/run/xl2tpd/l2tp-control")
 
-	output = os.popen("ip route").read()
-	# import re
+    # Getting chip IP
+    output = os.popen("ifconfig ppp0").read()
 
-	# pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    import re
 
-	# match = re.search(pattern, output)
-	# if match:
-	# 	ip_local = match.group()
+    pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
 
-	# os.system(f"route add {VPN_SERVER_IP} gw {ip_local}")
+    match = re.search(pattern, output)
+    if match:
+        ip_local = match.group()
 
-	os.system("route add default dev ppp1")
-	os.system("wget -qO- http://ipv4.icanhazip.com; echo")
+    os.system(f"route add {VPN_SERVER_IP} gw {ip_local}")
+    os.system("")
+    
+    # os.system("r")
+
+    # os.system("route add default dev ppp0")
+    os.system("route add default dev ppp1")
+    os.system("wget -qO- http://ipv4.icanhazip.com; echo")
 
 
 if __name__ == '__main__':
-	main()
+    main()
