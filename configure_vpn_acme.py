5c052c5e6e61325fc37eda9c88ca1f65bb943a85
Del Piero Flores <delpiero22.flores@gmail.com>
2023-01-03 17:21:32 -0500

    u

---
+++ configure_vpn_acme.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import time
+import re
+import subprocess
+# from datetime import date
+
+
+VPN_SERVER_IP='137.184.105.94' # ACME VPN IP
+
+CONNECTION = "chip"     # "chip" para datos moviles o "wifi" para la conexion a un router
+
+def get_ip_string(text):
+    pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+
+    match = re.search(pattern, text)
+    
+    return match.group()
+
+def main():
+
+   
+
+    # print("connecting VPN")
+
+    # Do it every reboot (we'll put it in /etc/rc.local)
+    os.system("mkdir -p /var/run/xl2tpd")
+    os.system("touch /var/run/xl2tpd/l2tp-control")
+
+    os.system("service strongswan restart")
+    os.system("ipsec restart")
+    os.system("service xl2tpd restart")
+
+    os.system("ipsec up myvpn")
+    os.system("echo \"c myvpn\" > /var/run/xl2tpd/l2tp-control")
+
+    time.sleep(10)
+
+    # Getting chip IP
+    
+    if CONNECTION == "chip":
+        output = os.popen("ifconfig ppp0").read()   #chip
+    
+    if CONNECTION == "wifi":
+        output = os.popen("ip route").read()      #network   
+    
+    
+    ip_local = get_ip_string(output)
+
+    # print("IP del chip:", ip_local)
+
+    
+    os.system(f"route add {VPN_SERVER_IP} gw {ip_local}")
+    
+    time.sleep(10)
+   
+
+    if CONNECTION == "wifi":
+        os.system("route add default dev ppp0")
+        time.sleep(30)
+    
+    if CONNECTION == "chip":
+        os.system("route add default dev ppp1")
+        time.sleep(30)
+
+    
+    vpn_ip_result = os.system("wget -qO- http://ipv4.icanhazip.com; echo")
+    vpn_ip_result_raw = os.popen("wget -qO- http://ipv4.icanhazip.com; echo").read()
+    
+    vpn_ip_result = get_ip_string(vpn_ip_result_raw)
+
+
+    if vpn_ip_result != VPN_SERVER_IP:
+        sys.exit(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+
+    except:
+        sys.exit(1)
